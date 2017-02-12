9a49ca939595139539def5b524621ebfbe4b704c
Faylixe <felix.voituret@gmail.com>
2017-02-12 18:21:50 +0100

    Updates to 1.1 for distribution

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from distutils.core import setup
 setup(
     name = 'pygame_vkeyboard',
     packages = ['pygame_vkeyboard'], # this must be the same as the name above
-    version = '1.0',
+    version = '1.1',
     description = 'Visual keyboard for Pygame engine. Aims to be easy to use as highly customizable as well.',
     author = 'Félix Voituret',
     author_email = 'felix.voituret@gmail.com',
     url = 'https://github.com/Faylixe/pygame_vkeyboard',
-    download_url = 'https://github.com/Faylixe/pygame_vkeyboard/tarball/1.0',
+    download_url = 'https://github.com/Faylixe/pygame_vkeyboard/tarball/1.1',
     install_requires=[
         'pygame',
     ],
