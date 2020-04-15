6110644112b96629824eebd529038c059c077615
Félix Voituret <fvoituret@deezer.com>
2020-04-15 12:01:00 +0200

    fix: bump version

--- setup.py
+++ setup.py
@@ -7,9 +7,12 @@ from distutils.core import setup
 
 setup(
     name='pygame_vkeyboard',
-    packages=['pygame_vkeyboard'], # this must be the same as the name above
-    version='1.1',
-    description='Visual keyboard for Pygame engine. Aims to be easy to use as highly customizable as well.',
+    packages=['pygame_vkeyboard'],
+    version='2.0.0',
+    description='''
+        Visual keyboard for Pygame engine. Aims to be easy to use as
+        highly customizable as well.
+    ''',
     author='Felix Voituret',
     author_email='felix.voituret@gmail.com',
     url='https://github.com/Faylixe/pygame_vkeyboard',
@@ -19,7 +22,8 @@ setup(
     ],
     keywords=['pygame', 'keyboard'],
     classifiers=[
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: pygame'
     ],
     include_package_data=True
