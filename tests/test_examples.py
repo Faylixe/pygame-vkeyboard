50bfabe1251ba8274bd2bd6897d8dd544a7e647e
anxuae <anxuae-prog@yahoo.fr>
2020-04-28 12:38:18 +0200

    Add simple unittests

---
+++ tests/test_examples.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf8
+
+from pygame_vkeyboard.examples import azerty, numeric, textinput
+
+
+def test_example_azerty():
+    azerty.main(test=True)
+
+
+def test_example_numeric():
+    numeric.main(test=True)
+
+
+def test_example_textinput():
+    textinput.main(test=True)
